ff51ce5e57d001a09fd277909aa8b1bb20120488
mlg3672 <michele.goe@gmail.com>
2015-06-29 13:37:23 +0800

    added lecture notes, code

--- week4.R
+++ week4.R
@@ -44,4 +44,58 @@ sqrt(sum((pred2V-validation$wage)^2))
 sqrt(sum((combPredV-validation$wage)^2)) # doesn't work!
 
 # Lecture 26 - Forecasting ----------
+library(quantmod)
+from.dat <- as.Date("01/01/08",format = "%m/%d/%y")
+to.dat<- as.Date("12/31/13",format = "%m/%d/%y")
+getSymbols("GOOG",src = "google", from = from.dat, to= to.dat)
+head(GOOG)
+index(GOOG)<-as.POSIXct(index(GOOG))
+mGOOg <- to.monthly(GOOG[,-5]) 
+googOpen <-Op(mGOOg)
+ts1 <- ts(googOpen, frequency = 12)
+plot(ts1,xlab="Years+1",ylab = "GOOG")
+plot(decompose(ts1),xlab="Years+1")
+
+# moving average
+library(forecast)
+ts1Train<-window(ts1,start=1,end=5)
+ts1Test<-window(ts1,start=5,end=(7-0.01))
+plot(ts1Train)
+lines(ma(ts1Train,order=3),col="red")
+
+# exponential smoothing
+ets1 <- ets(ts1Train,model="MMM")
+fcast <-forecast(ets1)
+plot(fcast,col="black")
+lines(ts1Test,col="red")
+# accuracy of forecast RMSE etc.
+accuracy(fcast,ts1Test)
+
+# Notes from book ---
+# 
+# Moving Averages of Moving Averages 
+#   m = 4 quarterly data
+#   m = 12 monthly data
+#   m = 7 weekly data
+# see x12 package for X-12-ARIMA decomposition
+
 # Lecture 27 - Unsupervised Prediction -----
+# Key Ideas : when don't know the labels of predictors...
+#             (1) build predictor by creating, naming clusters
+#             (2) in a new dataset predict clusters
+# exploratory technique, beware of overinterpretation
+library(caret)
+library(ggplot2)
+data(iris)
+inTrain<-createDataPartition(y= iris$Species, p=0.7, list=FALSE)
+training<-iris[inTrain,]
+testing<-iris[-inTrain,]
+dim(training); dim(testing)
+kMeans1<- kmeans(subset(training, select=-c(Species)),centers=3)
+training$clusters<- as.factor(kMeans1$cluster)
+qplot(Petal.Width, Petal.Length, colour=clusters, data=training)
+table(kMeans1$cluster,training$Species)
+modFit<-train(clusters~.,method="rpart", data=subset(training, select=-c(Species)))
+table(predict(modFit,training),training$Species)
+testClusterPred<-predict(modFit,testing)
+table(testClusterPred,testing$Species)
